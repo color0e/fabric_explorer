cd8b6e87ba50e291cc0f7af516b055e81455c038
color0e <limjangsoon@naver.com>
2019-03-06 22:05:10 +0900

    [fabric_util] invoke , query commit

--- web/lib/fabric_util.js
+++ web/lib/fabric_util.js
@@ -293,6 +293,158 @@ console.log(request);
 		
 	    }
 	}
+
+	fabric_util.getChaincodeRequest = function(chaincodeId, txId, fnc, chainId, args){
+        function IsTypeString(value){
+            if(value && (typeof value == "string")){
+                return true;
+            }else{
+                console.error("type not match STRING");
+                return false;
+            }
+        }
+        function IsTypeArray(value){
+            if(value.constructor === Array){
+                return true;
+            }else{
+                console.error("type not match ARRAY");
+                return false;
+            }
+        }
+        if(IsTypeString(chaincodeId) && IsTypeString(fnc) && IsTypeString(chainId)  && IsTypeArray(args)){
+            return {
+                'chaincodeId': chaincodeId,
+                'txId': txId,
+                'fcn': fnc,
+                'chainId': chainId,
+                'args': args
+            };
+        }else {
+            return {};
+        }
+      
+    }
+
+	fabric_util.common = function(request,isOnlyQuery){
+
+		return fabric_util.getAdminContext().then((user_from_store)=>{
+			if(isOnlyQuery){
+
+				return channel.queryByChaincode(request);
+		
+			}else{
+		
+				tx_id = fabric_client.newTransactionID();
+				request.txId = tx_id;
+
+				return channel.sendTransactionProposal(request);
+		
+			}
+		});
+	}
+
+	fabric_util.invoke = function(request,handler,errhandler){
+		let isOnlyQuery = false;
+		return fabric_util.common(request,isOnlyQuery).then((results) => {
+            var proposalResponses = results[0];
+            var proposal = results[1];
+            let isProposalGood = false;
+
+            if (proposalResponses && proposalResponses[0].response && proposalResponses[0].response.status === 200) {
+                    isProposalGood = true;
+                    console.log('Transaction proposal was good');
+            } else {
+                console.error('Transaction proposal was bad');
+            }
+
+            if(isProposalGood){
+                var request = {
+                    proposalResponses: proposalResponses,
+                    proposal: proposal
+                };
+          
+                return txHandler(request, tx_id, channel, fabric_client);
+            }else{
+                console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+                throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+            }
+            
+        }).then((results) => {
+            return handler(results, tx_id);
+        }).catch((err) => {
+            return errhandler(err);
+        });
+
+        function txHandler(request, tx_id, channel, fabric_client){
+	
+			// set the transaction listener and set a timeout of 30 sec
+			// if the transaction did not get committed within the timeout period,
+			// report a TIMEOUT status
+			var transaction_id_string = tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
+			var promises = [];
+			
+			// console.log(request);
+			console.log(channel);
+			var sendPromise = channel.sendTransaction(request);
+			// sendPromise.catch((e)=>{
+			// 	console.log(e);
+			// })
+			promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
+			
+			// get an eventhub once the fabric client has a user assigned. The user
+			// is required bacause the event registration must be signed
+			
+			//Error 발생했음 버전상승에 따라서 변경함
+			//let event_hub = fabric_client.newEventHub();
+			//event_hub.setPeerAddr('grpc://192.168.99.100:7053');
+			let event_hub = channel.newChannelEventHub(peer);
+			// using resolve the promise so that result status may be processed
+			// under the then clause rather than having the catch clause process
+			// the status
+			let txPromise = new Promise((resolve, reject) => {
+				let handle = setTimeout(() => {
+					event_hub.disconnect();
+					resolve({event_status : 'TIMEOUT'}); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
+				}, 3000);
+
+				event_hub.connect();
+
+				event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
+					// this is the callback for transaction event status
+					// first some clean up of event listener
+					clearTimeout(handle);
+					event_hub.unregisterTxEvent(transaction_id_string);
+					event_hub.disconnect();
+
+					// now let the application know what happened
+					var return_status = {event_status : code, tx_id : transaction_id_string};
+					if (code !== 'VALID') {
+						console.error('The transaction was invalid, code = ' + code);
+						resolve(return_status); // we could use reject(new Error('Problem with the tranaction, event status ::'+code));
+					} else {
+						console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
+						resolve(return_status);
+					}
+				}, (err) => {
+					//this is the callback if something goes wrong with the event registration or processing
+					reject(new Error('There was a problem with the eventhub ::'+err));
+				});
+			});
+			promises.push(txPromise);
+			console.log(promises);
+			return Promise.all(promises);
+		
+	    }
+	}
+
+	fabric_util.query = function(request,handler,errhandler){
+		let isOnlyQuery = true;
+		return fabric_util.common(request,isOnlyQuery).then((query_responses)=>{
+			handler(query_responses);
+		}).catch((err)=>{
+			errhandler(err);
+		})
+	}
 	
 
 	//디렉토리내 모든 파일을 읽음
